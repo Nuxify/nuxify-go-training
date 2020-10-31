54372167f5762d9a8c72dce9a199bb560e312467
Loed Membreve <loedy26@gmail.com>
2020-10-31 08:11:07 +0800

    feat(main): "Adding http request"

--- 2-rest-client/main.go
+++ 2-rest-client/main.go
@@ -4,8 +4,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
+
+type values struct {
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	UserID int    `json:"userId"`
+}
 
 func main() {
 	CreatePost()
@@ -30,9 +38,6 @@ func CreatePost() {
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("Status Code", resp.StatusCode)
-	fmt.Println("Body", resp.Body)
-
 	var response map[string]interface{}
 
 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
@@ -45,17 +50,67 @@ func CreatePost() {
 
 // DeleteByID delete id
 func DeleteByID() {
+	values := values{"GarzAlma", "bar", 1}
+	jsonReq, err := json.Marshal(values)
+	req, err := http.NewRequest(http.MethodDelete, "https://jsonplaceholder.typicode.com/posts/1", bytes.NewBuffer(jsonReq))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	// Convert response body to string
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
 }
 
 // GetPostByID get post by id
 func GetPostByID() {
 
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	// Convert response body to string
+	bodyString := string(bodyBytes)
+	fmt.Println("API Response as String:\n" + bodyString)
+
+	// Convert response body to map interface
+	var response map[string]interface{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("POST REQUEST:", response)
 }
 
 // GetAllPosts get all posts
 func GetAllPosts() {
 
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
 
 // UpdatePost update post by id
