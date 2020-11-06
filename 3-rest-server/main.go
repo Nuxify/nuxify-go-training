c788c64579768bd58df5ebebaa3e11476fb58786
Loed Membreve <loedy26@gmail.com>
2020-11-06 20:50:45 +0800

    feat(main): "create temp handle request"

--- 3-rest-server/main.go
+++ 3-rest-server/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
-	http.ListenAndServe(":3000", r)
+	fmt.Println("Starting Server....")
+
+	router := chi.NewRouter()
+	router.Get("/api/getExample", getHandler)
+	router.Post("/api/createExample", postHandler)
+	router.Patch("/api/updateExample", patchHandler)
+	router.Delete("/api/deleteExample", delhHandler)
+	fmt.Println("Server is listening on port 8080....")
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+func delhHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Your request has been deleted!")
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("You got me a GET Request!")
+}
+
+func patchHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Your request has been updated!")
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("You just sent me a post request!")
 }
