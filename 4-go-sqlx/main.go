1f948fc452f8a68ffab4e00696f2c6452fa29aad
Loed Membreve <loedy26@gmail.com>
2020-11-13 21:38:53 +0800

    feat(main): test createUser func

--- 4-go-sqlx/main.go
+++ 4-go-sqlx/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -23,7 +24,25 @@ func main() {
 
 	db.Select(&user, "select * from users")
 
+	// createUser()
+
 	log.Println("users...")
 	log.Println(user)
 
+}
+
+func createUser() {
+	db, err := sqlx.Connect("mysql", "root:1234@(localhost:3306)/test_sqlx")
+	if err != nil {
+		panic(err)
+	}
+	res, err := db.Exec("INSERT INTO users (name) VALUES(\"Additional test user\")")
+	if err != nil {
+		panic(err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created user with id:%d", id)
 }
